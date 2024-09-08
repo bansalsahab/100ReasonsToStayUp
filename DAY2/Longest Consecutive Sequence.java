6e74d03f64dd2db54086c4e2749b2739a54f11bf
Parth bansal <120677173+bansalsahab@users.noreply.github.com>
2024-09-08 15:23:29 +0530

    Update Longest Consecutive Sequence.java

--- DAY2/Longest Consecutive Sequence.java
+++ DAY2/Longest Consecutive Sequence.java
@@ -1,1 +1,23 @@
 
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for(int num : nums){
+            set.add(num);
+        }
+        int maxLen = 0;
+        for(Integer element : set){
+            int prevEl = element-1;
+            if(!set.contains(prevEl)){
+                int len =1;
+                int nextEl = element+1;
+                while(set.contains(nextEl)){
+                    len++;
+                    nextEl++;
+                }
+                maxLen = Math.max(maxLen , len);
+            }
+        }
+        return maxLen;
+    }
+}
