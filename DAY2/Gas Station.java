995ff601be1cb228632bd4c098add48b71c12a97
Parth bansal <120677173+bansalsahab@users.noreply.github.com>
2024-09-08 18:59:20 +0530

    Create Gas Station.java

---
+++ DAY2/Gas Station.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int totalGas = 0;
+        int totalCost = 0; 
+        int start = 0;
+        int tank = 0;
+
+        for (int i = 0; i < gas.length; i++) {
+            totalGas += gas[i];
+            totalCost += cost[i];
+            tank += gas[i] - cost[i];
+            if(tank <0){
+                start = i+1;
+                tank =0;
+            }
+        }
+        return totalGas<totalCost? -1 : start;
+    }
+}
