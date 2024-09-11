10f77f3af3222489024116432c365ed0b0358dfa
Parth bansal <120677173+bansalsahab@users.noreply.github.com>
2024-09-12 03:10:25 +0530

    Create Third Maximum Number.java

---
+++ DAY5/Third Maximum Number.java
@@ -0,0 +1,30 @@
+public class ThirdMaximumNumber {
+    public static int thirdMax(int[] nums) {
+        long first = Long.MIN_VALUE;
+        long second = Long.MIN_VALUE;
+        long third = Long.MIN_VALUE;
+
+        boolean hasThird = false;
+
+        for (int num : nums) {
+
+            if (num == first || num == second || num == third) {
+                continue;
+            }
+            if (num > first) {
+                third = second;
+                second = first;
+                first = num;
+            } else if (num > second) {
+                third = second;
+                second = num;
+            } else if (num > third) {
+                third = num;
+                hasThird = true;
+            }
+        }
+
+        return hasThird ? (int) third : (int) first;
+    }
+}
+  
