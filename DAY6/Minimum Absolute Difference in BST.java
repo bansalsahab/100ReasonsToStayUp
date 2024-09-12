e50bb1a5546585bcbdb3b52d7849604ef3663b7a
Parth bansal <120677173+bansalsahab@users.noreply.github.com>
2024-09-13 00:54:28 +0530

    Create Minimum Absolute Difference in BST.java

---
+++ DAY6/Minimum Absolute Difference in BST.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int getMinimumDifference(TreeNode root) {
+        List<Integer> nodeList = new ArrayList<>();
+        inOrderTraversal(root, nodeList);
+        
+        int minDifference = Integer.MAX_VALUE;
+        for (int i = 1; i < nodeList.size(); i++) {
+            minDifference = Math.min(minDifference, nodeList.get(i) - nodeList.get(i - 1));
+        }
+        
+        return minDifference;
+    }
+    private void inOrderTraversal(TreeNode node, List<Integer> nodeList) {
+        if (node == null) return;
+        
+        inOrderTraversal(node.left, nodeList); 
+        nodeList.add(node.val); 
+        inOrderTraversal(node.right, nodeList); 
+    }
+}
