ffe7442430f7ac5a5d486c68112b0b3e1e68f57a
Parth bansal <120677173+bansalsahab@users.noreply.github.com>
2024-09-14 01:23:31 +0530

    Create Smallest Subsequence of Distinct Characters.java

---
+++ DAY7/Smallest Subsequence of Distinct Characters.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String smallestSubsequence(String s) {
+        int n =s.length();
+        StringBuilder result =new StringBuilder();
+        boolean taken[] =  new boolean[26];
+        int lastIndex[] = new int[26];
+
+        for(int i =0 ;i<n;i++){
+            char ch = s.charAt(i);
+            lastIndex[ ch - 'a'] = i; 
+        }
+        for(int i =0 ;i<n ;i++){
+            char ch = s.charAt(i);
+            int idx = ch - 'a';
+
+            if(taken[idx]) continue;
+            // last index wala bada hona chaiye or bad m ana chaiye
+            while(result.length() > 0 && ch<result.charAt(result.length()-1) && lastIndex[result.charAt(result.length()-1) -'a']>i){
+                taken[result.charAt(result.length() - 1) - 'a'] = false;
+                result.deleteCharAt(result.length() - 1);
+            }
+            result.append(ch);
+            taken[idx]= true;
+        }
+        return result.toString();
+    }
+}
