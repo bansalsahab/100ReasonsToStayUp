fc10d1cad1589ad0db78a059a418f798e2557677
Parth bansal <120677173+bansalsahab@users.noreply.github.com>
2024-09-17 00:13:58 +0530

    Create 4Sum.java

---
+++ DAY9/4Sum.java
@@ -0,0 +1,32 @@
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        List<List<Integer>> ans =  new ArrayList<>();
+        for(int i =0 ;i< nums.length;i++){
+            if(i > 0 && nums[i] == nums[i-1]) continue;
+
+            for(int j =i  + 1; j<nums.length;j++){
+                if(j != (i + 1) && nums[j] == nums[j-1]) continue;
+                int k = j+1;
+                int l = n-1;
+                while(k< l){
+                    long sum =nums[i];
+                    sum += nums[j];
+                    sum += nums[k];
+                    sum += nums[l];
+                    if(sum == target){
+                        ans.add(Arrays.asList(nums[i], nums[j], nums[k], nums[l]));
+                        k++;
+                        l--;
+                        while(k < l && nums[k] == nums[k-1]) k++;   
+                        while(k < l && nums[l] == nums[l+1]) l--;
+                    }
+                    else if(sum < target )k++;
+                    else l--;
+                }
+            }
+        }
+        return ans;
+    }
+}
